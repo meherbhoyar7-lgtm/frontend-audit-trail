88b56b9806d577797d073b966a40bb884e745f88
meherbhoyar7-lgtm <meherbhoyar7@gmail.com>
2026-06-15 17:36:42 +0530

    v2

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000', // FastAPI Gateway
+    baseURL: 'https://gateway-2a4x.onrender.com', // FastAPI Gateway
     headers: {
         'Content-Type': 'application/json',
     },
